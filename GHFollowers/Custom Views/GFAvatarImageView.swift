f56b6bd34abcde9f8c76ee24fa827461c1288cc6
Andrii Kuznietsov <awesomeakm@gmail.com>
2023-11-13 21:43:19 +0200

    Download and cache images

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(resource: .avatarPlaceholder)
 
     override init(frame: CGRect) {
@@ -27,5 +28,32 @@ class GFAvatarImageView: UIImageView {
         image = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
     }
-
+    
+    
+    func downloadImage(from urlString: String) {
+        
+        let cachekey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cachekey) {
+            self.image = image
+            return
+        }
+        
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            cache.setObject(image, forKey: cachekey)
+            
+            DispatchQueue.main.async { self.image = image }
+        }
+        
+        task.resume()
+    }
 }
