7e77b9aa6aff55ff7dfeae44cce21163a7f2d552
Andrii Kuznietsov <awesomeakm@gmail.com>
2023-11-30 18:02:33 +0200

    Configure ItemInfoVC and place its subclasses to User Info screen

---
+++ GHFollowers/CustomViews/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Andrii Kuznietsov on 30.11.2023.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+    
+}
